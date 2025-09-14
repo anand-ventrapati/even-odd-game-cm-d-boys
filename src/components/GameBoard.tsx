57c4f55d98cf000c6bd40554e764a259671bda94
Anand Ventrapati <anand.ventrapati@gmail.com>
2025-09-14 18:00:52 +0530

    Update GameBoard.tsx

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -279,7 +279,7 @@ const GameBoard = ({ gameState, onUpdateGame, localPlayerId }: GameBoardProps) =
                   <div>
                     <label className="block text-white mb-2">Choose Number (0-9)</label>
                     <div className="grid grid-cols-5 gap-2">
-                      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(num => (
+                      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(num => (
                         <Button
                           key={num}
                           onClick={() => setBetNumber(num)}
